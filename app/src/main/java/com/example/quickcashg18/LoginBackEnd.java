acd98c27c2d8faf482fbc7a9ad509cc7a434a57c
saidi <ib409215@dal.ca>
2022-10-02 20:13:12 -0300

    Firebase connected

---
+++ app/src/main/java/com/example/quickcashg18/LoginBackEnd.java
@@ -0,0 +1,22 @@
+package com.example.quickcashg18;
+
+import android.content.SharedPreferences;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class LoginBackEnd {
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference firebaseDatabaseRef;
+    private static final String FIREBASE_DB_URL = "https://quick-cash-g18-default-rtdb.firebaseio.com/";
+    SharedPreferences userSharedPreferences;
+    SharedPreferences.Editor sharePreferenceEditor;
+
+
+
+
+}
