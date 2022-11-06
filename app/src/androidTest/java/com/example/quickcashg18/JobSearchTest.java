e0b63acaa4ab2c03d5a04730ff8aaee7aeefea4c
sb670504 <sb670504@dal.ca>
2022-11-06 18:15:54 -0400

    add test class

---
+++ app/src/androidTest/java/com/example/quickcashg18/JobSearchTest.java
@@ -0,0 +1,35 @@
+package com.example.quickcashg18;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class JobSearchTest {
+
+    @Rule
+    public ActivityScenarioRule<job_search> myRule = new ActivityScenarioRule<>(job_search.class);
+
+    @BeforeClass
+    public static void setup() { Intents.init(); }
+
+    @AfterClass
+    public static void tearDown() {
+        System.gc();
+    }
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.quickcashg18", appContext.getPackageName());
+    }
+}
