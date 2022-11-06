47845d32328ac46e8c2f8c3f977f52f697b8e10e
sb670504 <sb670504@dal.ca>
2022-11-06 18:16:51 -0400

    job search backend

--- app/src/main/java/com/example/quickcashg18/job_search.java
+++ app/src/main/java/com/example/quickcashg18/job_search.java
@@ -3,20 +3,57 @@ package com.example.quickcashg18;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class job_search extends AppCompatActivity {
+
+    SearchView searchView;
+    ListView listView;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_search);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.search_menu, menu);
-        return true;
+
+        searchView = findViewById(R.id.searchView);
+        listView = findViewById(R.id.list_view);
+
+        list = new ArrayList<String>();
+        list.add("Walking dog");
+        list.add("Job Hunter");
+        list.add("Window Washing");
+        list.add("Baby sitting");
+        list.add("Car cleaner");
+        list.add("Movers");
+        list.add("Furniture assembler");
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                if (list.contains(s)) {
+                    adapter.getFilter().filter(s);
+                } else {
+                    // Search query not found in List View
+                    Toast.makeText(job_search.this, "Not found", Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                adapter.getFilter().filter(s);
+
+                return false;
+            }
+        });
     }
 }
