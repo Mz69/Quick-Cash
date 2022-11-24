b695defb750e27ad92ff4a9c5a1d54f7f5cbf0ba
AngelaDaurie <an533546@dal.ca>
2022-11-24 18:31:36 -0400

    basic function for paypal implemented, tip not account for yet and job details not done yet

--- app/src/androidTest/java/com/example/quickcashg18/paymentTests.java
+++ app/src/androidTest/java/com/example/quickcashg18/paymentTests.java
@@ -1,2 +1,19 @@
-package com.example.quickcashg18;public class paymentTests {
+package com.example.quickcashg18;
+
+import static org.junit.Assert.assertEquals;
+import android.content.Context;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+public class paymentTests {
+    //test for empty payment (fail)
+    //test for correct payment (pass)
+    //test for too small an amount (fail)
+
 }
